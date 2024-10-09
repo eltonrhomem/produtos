abfdfb99c5585674cdc69a099b50578b7501ef7f
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2024-10-09 11:24:48 -0300

    faz traducao

--- src/main/java/br/com/alura/Main.java
+++ src/main/java/br/com/alura/Main.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import br.com.alura.model.Produto;
+import br.com.alura.service.TraduzProdutoService;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,5 +17,13 @@ public class Main {
         for(Produto produto: produtos) {
             System.out.println(produto);
         }
+
+        TraduzProdutoService traducaoService = new TraduzProdutoService();
+
+        for(Produto produto: produtos) {
+            traducaoService.traduzir(produto);
+            System.out.println(produto);
+        }
+        
     }
 }
--- src/main/java/br/com/alura/service/TraduzProdutoService.java
+++ src/main/java/br/com/alura/service/TraduzProdutoService.java
@@ -1,12 +1,16 @@
 package br.com.alura.service;
 
 import br.com.alura.model.Produto;
-
 import javax.swing.*;
-
 
 public class TraduzProdutoService {
     public void traduzir(Produto produto) {
-        String nome = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getName());
+        String novoNome = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getName());
+        String novoCategoria = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getCategory());
+        String novoDescricao = Translator.translate(Language.ENGLISH, Language.PORTUGUESE, produto.getDescription());
+
+        produto.setName(novoNome);
+        produto.setCategory(novoCategoria);
+        produto.setDescription(novoDescricao);
     }
 }
